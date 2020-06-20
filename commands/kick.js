cc4acf2ce92da7edb9cd55ad9c9e6c9302edd091
Max Thakur <Dominick2024204@concordiashanghai.org>
2020-06-20 18:53:27 -0400

    Adding a placeholder

---
+++ commands/kick.js
@@ -0,0 +1,7 @@
+module.exports.run = (client, message, args) => {
+  message.channel.send("This command is being reworked, please join out support server for updates on boyfriend bot! https://discord.gg/hBfmxVt");
+}
+
+module.exports.help = {
+  name: "purge"
+}
