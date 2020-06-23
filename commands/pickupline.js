4f60b4a2032e0ba96f0693ff74b0d942e075d16f
Max Thakur <MaxThakurCodes@gmail.com>
2020-06-23 11:00:10 -0400

    Adding PickupLines

--- commands/pickupline.js
+++ commands/pickupline.js
@@ -9,10 +9,12 @@ const cevaplar = [
   'Feel my shirt. Know what its made of? Boyfriend material.',
   'Did your license get suspended for driving all these guys crazy?',
   'Baby, if you were words on a page, youd be fine print.',
-  'Did you just come out of the oven? Because youre hot.',
+  'Did you just come out of the oven? Because you\'re hot.',
   'Its a good thing I have my library card, because I am totally checking you out.',
   'I was feeling a little off today—but youve turned me on again!',
   'Im studying to become a historian. Im especially interested in finding a date.',
+  'Boy: "How many planets are there"\nGirl: "8"\nBoy: "No 7 because im gonna smash Uranus tonight"\nGirl: ":flushed:"',
+  'Boy: "Spell ME"\nGirl: "M E"\nBoy: "You spelt it wrong"\nGirl: ":confused:"\nBoy: "It\'s me d in you v"'
 ];
 exports.run = function (client, message, args) {
   var soru = args.join(' ');
