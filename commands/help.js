d08aedeecd7f41e853f533cc867e865ebe5e0ea4
robbie <66927283+robbiesrude@users.noreply.github.com>
2020-06-24 23:35:20 -0700

    help command fixes

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ exports.run = async (client, msg, args) => {
     .setColor('#7289DA')
     .setAuthor('𝐁𝐎𝐘𝐅𝐑𝐈𝐄𝐍𝐃 𝐁𝐎𝐓 𝐀𝐋𝐏𝐇𝐀 - 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒', "https://www.freepnglogos.com/uploads/heart-png/emoji-heart-33.png")
     .addField(':see_no_evil: Boyfriend Bot Fun:', '*-boyfriend, -slap, -kiss, -date*')
-    .addField(':partying_face: General Fun:', '*-pickuplines, -playtruth, -playdare*')
+    .addField(':partying_face: General Fun:', '*-pickuplines, -gametruth, -gamedare, -truthordare*')
     .addField(':ok_hand: Utility:', '*-help, -avatar (@user), -check (@user), -updatelog, -invite, -info (Information on bot), -suggest, -vote, -credits*')
     .addField(':cop: Moderation: (NOT WORKING DOWN FOR REWORKING)', '-kick, -ban, -purge')
     .addField(':sparkles: Help Support the Bot!', '*Feel free to vote for the bot to help us get noticed by more people! https://bit.ly/BoyfriendVote*')
