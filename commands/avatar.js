f4ed8dbbcc6e0df0d6eb251b582bb7342ec54c2e
Max Thakur <MaxThakurCodes@gmail.com>
2020-06-26 19:55:34 -0400

    adding await

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,23 +1,23 @@
 const Discord = require("discord.js");
 
-module.exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
 
   let user = message.mentions.users.first() || message.author;
   let embed = new Discord.MessageEmbed()
     .setDescription(`Damn whos that qt? <@${user.id}>`)
-    .setAuthor(`${user.username}#${user.discriminator}`, client.user.avatarURL({
+    .setAuthor(`${user.username}#${user.discriminator}`, await client.user.avatarURL({
       format: "png",
       dynamic: true,
       size: 2048
     }))
-    .setImage(user.avatarURL({
+    .setImage(await user.avatarURL({
       format: "png",
       dynamic: true,
       size: 2048
     }))
     .setColor('#7289DA')
     .setTimestamp()
-    .setFooter("Requested by " + message.author.tag, message.author.avatarURL({
+    .setFooter("Requested by " + message.author.tag, await message.author.avatarURL({
       format: "png",
       dynamic: true,
       size: 2048
