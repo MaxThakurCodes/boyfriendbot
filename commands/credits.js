275e1cfffbc88e5003ff39121fe98a798bf78783
Max Thakur <MaxThakurCodes@gmail.com>
2020-06-27 00:15:07 -0400

    changed italic to users who's Id's are unknown.

--- commands/credits.js
+++ commands/credits.js
@@ -6,7 +6,7 @@ exports.run = async (client, msg, args) => {
     .setAuthor('Boyfriend Bot Alpha Contributors')
     .addField(':thinking: What is this?', '*This is a list of people who helped to develop boyfriend bot and make it what it is today.*') 
     .addField(':desktop: Developer Credits', '*robbie#2544 (Creator), Max Thakur#0831 (Rewritten for D.js v12)*')    
-    .addField(':pencil: Suggestion Credits:', '<@329887126831497216>, 🍼ʙᴀʙʏ🍼#5501, Piggy#9713, <@315534526988156928>, <@530279594016636938>*')
+    .addField(':pencil: Suggestion Credits:', '<@329887126831497216>, *🍼ʙᴀʙʏ🍼#5501, Piggy#9713*, <@315534526988156928>, <@530279594016636938>')
     .addField(':selfie: Boyfriend Bot Profile Picture:', '*Tony Lopez (@tony_lopez IG)*')
     .addField(':person_raising_hand: Want your name in the credits?', '*Fill out our suggestion google form! https://forms.gle/DihjEbVfKqUmVSJt6*')
     .addField(':sparkles: Help Support the Bot!', '*Feel free to vote for the bot to help us get noticed by more people! https://bit.ly/BoyfriendVote*')
