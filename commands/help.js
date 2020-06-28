137a0682f5ab00f5d76643099e28329c5a377142
Max Thakur <Dominick2024204@concordiashanghai.org>
2020-06-28 07:06:31 -0400

    Update help.js
    Hot fixes

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,21 @@
 const Discord = require("discord.js");
+const Server = require("../models/server")
 
 exports.run = async (client, msg, args) => {
+  let fS = await Server.findOne({ guildId: msg.guild.id })
+  let prefix;
+  if (fS === null) {
+    prefix = "-"
+  } else {
+    prefix = fs.prefix
+  }
   let embed = new Discord.MessageEmbed()
     .setColor('#7289DA')
     .setAuthor('Boyfriend Bot Commands', "https://www.freepnglogos.com/uploads/heart-png/emoji-heart-33.png")
     .addField(':speak_no_evil: Boyfriend Bot Fun:', '*-boyfriend, -slap, -kiss, -date*')
     .addField(':partying_face: General Fun:', '*-pickupline, -nsfwpickuplines (Must be used in NSFW marked channel), -gametruth, -gamedare, -truthordare*')
-    .addField(':ok_hand: Utility:', '*-help, -icon, -avatar (@user), -check (@user), -updatelog, -invite, -info (Information on bot), -suggest, -credits*')
-    .addField(':cop: Moderation:', '*-kick, -ban, -purge*')
+    .addField(':ok_hand: Utility:', '*-help, -icon, -avatar (@user), -check (@user), -updatelog, -invite, -info, -suggest, -credits, -prefix*')
+    .addField(':cop: Moderation:', '*-kick, -ban, -purge, -sprefix (prefix)*')
     .addField(':sparkles: Help Support the Bot!', '*Feel free to vote for the bot to help us get noticed by more people! https://bit.ly/BoyfriendVote*')
     .addField(':fist_tone5: Support Black Lives Matter!', '*Boyfriend Bot developers support the Black Lives Matter Movement, if you wish to help go to https://blacklivesmatters.carrd.co/ and sign all petitions and find ways to help.*')
     .setFooter('𝐁𝐎𝐘𝐅𝐑𝐈𝐄𝐍𝐃 𝐁𝐎𝐓 - 𝐜𝐢𝐭𝐢𝐳𝐞𝐧𝐬 𝐝𝐞𝐯𝐞𝐥𝐨𝐩𝐦𝐞𝐧𝐭');
